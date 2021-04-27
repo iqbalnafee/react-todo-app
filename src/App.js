41cd65bf16fd8f8de2c4dfee92715d09b028a716
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-04-27 22:23:48 +0600

    form created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Form from './component/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>TODO List</h1>
+      </header>
+
+      <Form /> 
+    </div>
+  );
+}
+
+export default App;
